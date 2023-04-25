be843647c0ebd597f0524ec3067c8fe44dbec356
Mohamed Ahmed <ma07041705@gmail.com>
2023-04-25 18:17:52 +0300

    custom API added

---
+++ household/src/constant/Api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:5000";
+
+ const apiCall = async (url, method, data = {}, headers = {}) => {
+  const user = localStorage.getItem("user");
+  const token = user ? JSON.parse(user).token : null;
+  const config = {
+    headers: { Authorization: `${token}`, ...headers },
+  };
+
+  try {
+    const res = await axios({ method, url, data, baseURL: BASE_URL,...config, });
+    return res.data;
+  } catch (err) {
+    const error = err.response?.data?.message
+    console.log(error)
+    throw err;
+  }
+};
+
+
+export default apiCall;
--- household/src/views/User-Dashoard/Post.vue
+++ household/src/views/User-Dashoard/Post.vue
@@ -9,58 +9,30 @@
 
       <div class="fixed inset-0 bg-zinc-100 bg-opacity-75 blur" v-if="showModal"></div>
       
-      <div class="relative px-6 py-4" v-if="showModal">
+      <div class="relative px-6 py-40" v-if="showModal">
         <div class="absolute inset-0  flex items-center justify-center lg:top-20 top-20">
             <PostForm @close-modal="closeModal"/>
         </div>
       </div>
 
     <div class="max-w-md mx-auto bg-white rounded-md shadow-md overflow-hidden mt-5">
-        <div class="border-b mb-5">
+        <div class="border-b mb-5" v-for="blog in blogs" :key="blog.blog_id">
             <div class="flex items-center p-4">
                 <div class="w-12 h-12 mr-4">
-                <img src="../../assets/images/photo-1633332755192-727a05c4013d.jpg" alt="Product Image" class="w-full h-full rounded-full object-cover">
+                <img :src="blog.user.image" alt="Product Image" class="w-full h-full rounded-full object-cover">
                 </div>
                 <div class="">
-                    <h2 class="text-base font-bold mb-1">John</h2>
-                    <p class="text-gray-600 text-sm mb-2">1 day ago</p>
+                    <h2 class="text-base font-bold mb-1">{{blog.user.username}}</h2>
+                    <p class="text-gray-600 text-sm mb-2">{{ blog.date }}</p>
                 </div>
             </div>
             <div class="px-4 pb-4">
-                <p class="text-gray-800 text-sm leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus explicabo expedita enim incidunt ipsam voluptatibus tempora null</p>
-                <img src="../../assets/images/Babysitting-Mobile.jpg" class="h-48 w-full object-cover mt-4" />
+                <p class="text-gray-800 text-sm leading-6">{{ blog.description }}</p>
+                <img :src="blog.image" class="h-96 w-full object-cover mt-4" />
             </div>  
         </div>  
-        <div class="border-b  mb-5">
-            <div class="flex items-center p-4">
-                <div class="w-12 h-12 mr-4">
-                <img src="../../assets/images/photo-1633332755192-727a05c4013d.jpg" alt="Product Image" class="w-full h-full rounded-full object-cover">
-                </div>
-                <div class="">
-                    <h2 class="text-base font-bold mb-1">John</h2>
-                    <p class="text-gray-600 text-sm mb-2">1 day ago</p>
-                </div>
-            </div>
-            <div class="px-4 pb-4">
-                <p class="text-gray-800 text-sm leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus explicabo expedita enim incidunt ipsam voluptatibus tempora null</p>
-                <img src="../../assets/images/Babysitting-Mobile.jpg" class="h-48 w-full object-cover mt-4" />
-            </div> 
-        </div> 
-        <div class="border-b  mb-5">
-            <div class="flex items-center p-4">
-                <div class="w-12 h-12 mr-4">
-                <img src="../../assets/images/photo-1633332755192-727a05c4013d.jpg" alt="Product Image" class="w-full h-full rounded-full object-cover">
-                </div>
-                <div class="">
-                    <h2 class="text-base font-bold mb-1">John</h2>
-                    <p class="text-gray-600 text-sm mb-2">1 day ago</p>
-                </div>
-            </div>
-            <div class="px-4 pb-4">
-                <p class="text-gray-800 text-sm leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus explicabo expedita enim incidunt ipsam voluptatibus tempora null</p>
-                <img src="../../assets/images/Babysitting-Mobile.jpg" class="h-48 w-full object-cover mt-4" />
-            </div> 
-        </div> 
+     
+      
     </div>
 
 
@@ -68,28 +40,41 @@
 </template>
 
 <script>
-import  PostForm from '../../components/Create-Post-form.vue'
+import PostForm from '../../components/Create-Post-form.vue'
+import apiCall from '../../constant/Api'
+
 export default {
-    components: {
+  components: {
     PostForm,
   },
 
-  data(){
+  data() {
     return {
-        showModal: false,
+      blogs: [],
+      showModal: false,
     };
   },
- methods: {
+
+  created() {
+    apiCall('/Blogs', 'GET')
+      .then((res) => {
+        this.blogs = res.blogs; 
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  methods: {
     openModal() {
       this.showModal = true;
     },
+
     closeModal() {
       this.showModal = false;
     },
   },
-}
+};
 </script>
 
-<style>
 
-</style>
