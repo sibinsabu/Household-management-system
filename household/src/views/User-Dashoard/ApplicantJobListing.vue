57b0873bd711a4ca3508c537bffb84933db4ceb9
Mohamed Ahmed <ma07041705@gmail.com>
2023-05-11 18:11:10 +0300

    accepting Applicant

--- household/src/views/User-Dashoard/ApplicantJobListing.vue
+++ household/src/views/User-Dashoard/ApplicantJobListing.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-20 ">
-
     <div>
       <div class="border rounded-t-lg p-4 h-52">
          <h2 class="text-2xl font-bold mb-2 text-purple-800 uppercase text-center">job info</h2>
@@ -25,7 +24,7 @@
       </div>
       <div class="border bg-white rounded-b-lg p-4">
            <h2 class="text-2xl font-bold mb-2 text-purple-800 uppercase text-center">Applicant</h2>
-           <div v-for="ApplicantJob in ApplicantJobs" :key="ApplicantJob.job_id" class="border-b last:border-b-0 pt-3">
+           <div v-for="ApplicantJob in ApplicantJobs" :key="ApplicantJob.job_id" class="border-b last:border-b-0 mb-5 pt-3">
               <span class="text-lg font-bold uppercase">UserName: </span>
               <router-link :to="{ name: 'PublicProfile', params:{ id:ApplicantJob.user.id} }"><span class="text-lg font-medium text-purple-800 uppercase">{{ApplicantJob.user.username }}</span></router-link>
               <div class="mb-6 py-2">
@@ -33,7 +32,7 @@
                   <p class="text-gray-700 leading-relaxed">{{ApplicantJob.user.AboutMe }}</p>
               </div>
                 
-              <div class="mb-2">
+              <div>
                 <div class="grid grid-cols-2 gap-4">
                   <div>
                     <h4 class="text-gray-700 font-bold mb-1">Date Applied</h4>
@@ -57,10 +56,17 @@
                   </div>
                     </div>
               </div>
+
+              <div v-if="JobsListing.jobTitle === 'Posted'" class="mt-2 mb-10">
+              <p class="font-medium font-serif">Accept Application</p>
+              <button @click="AcceptApplication(ApplicantJob.user.id, JobsListing.job_id)" class="text-base bg-green-600 text-white px-2 py-1 rounded mr-3 capitalize">Accept</button>
+              </div>
+              <div else-if="JobsListing.jobTitle === 'Hired'" class="mt-2 mb-10">
+                <p class="font-medium font-serif text-red-400">Application is Closed</p>
+              </div>
            </div>
       </div>
     </div>
-    
   </div>
 </template>
 
@@ -88,6 +94,22 @@ export default {
       })
   },
   methods: {
+    AcceptApplication(user_id,job_id ) {
+    apiCall('/JobsApplication/AcceptApplicant', 'post', {
+       user_id:user_id,
+      })
+      .then((res) => {     
+          apiCall(`JobListings/${job_id}`, "PATCH", { status: 'Hired' })
+          .then((res) => {
+           console.log(res);
+      })
+      .catch((error) => {
+       console.log(error);
+      });
+      }) .catch((error) => {
+      });
+    },
+
     formatSalary(salary) {
         const salaryNum = Number(salary);
         if (salaryNum >= 10000) {
