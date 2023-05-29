f18568dc802160f6fbcb1f70f34f1af7c17e4e3a
Mohamed Ahmed <ma07041705@gmail.com>
2023-05-29 20:30:32 +0300

    @vercel/analytics

--- household/src/main.js
+++ household/src/main.js
@@ -7,7 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fab} from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { Analytics } from '@vercel/analytics';
+import { inject } from '@vercel/analytics';
 library.add(fab, fas)
 
 
@@ -24,5 +24,5 @@ router.beforeEach(() => {
 
 createApp(App)
 .component('fa', FontAwesomeIcon)
-.component('Analytics', Analytics)
+.component('inject ',inject)
 .use(store).use(router).mount('#app')
