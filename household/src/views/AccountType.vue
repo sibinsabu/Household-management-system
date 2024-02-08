7f6ba2bf2ca7009c4ded48067d42aeae6eb7b9eb
Mohamed Ahmed <ma07041705@gmail.com>
2024-02-08 17:35:18 +0300

    edited

--- household/src/views/AccountType.vue
+++ household/src/views/AccountType.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="max-w-7xl mx-auto px-4 my-60 lg:my-0">
-      <div class="flex flex-col lg:flex-row justify-center items-center h-5/6 lg:h-screen">
+      <div class="flex flex-col lg:flex-row justify-center items-center h-5/6 lg:h-screen gap-6">
         
         <a href="/Applicant" class="flex-1 lg:flex-none">
-          <div class="border rounded-lg p-6 mr-4 flex items-center justify-center lg:justify-start hover:bg-blue-100">
+          <div class="border rounded-lg p-6 mr-4 flex items-center justify-center lg:justify-start hover:bg-blue-50">
             <img src="../assets/images/bigstock-Hiring-Recruiting-Interview-114114056.webp" alt="Applicant Logo" class="w-60 h-60 mr-2">
             <div>
               <h2 class="text-lg font-bold mb-2">Applicant</h2>
@@ -13,11 +13,11 @@
         </a>
 
         <a href="/Homeowner" class="flex-1 lg:flex-none">
-          <div class="border rounded-lg p-6 flex items-center justify-center lg:justify-start hover:bg-blue-100">
-            <img src="../assets/images/homeowner-keys.jpg" alt="Applicant Logo" class="w-60 h-60 mr-2">
+          <div class="border rounded-lg p-6 mr-4 flex items-center justify-center lg:justify-start hover:bg-blue-50">
+                     <img src="../assets/images/homeowner-keys.jpg" alt="Applicant Logo" class="w-60 h-60 mr-2">
             <div>
               <h2 class="text-lg font-bold mb-2">Homeowner</h2>
-              <p>Content for the homeowner section goes here.</p>
+              <p>Content for the applicant section goes here.</p>
             </div>
           </div>
         </a>
--- household/src/views/User-Dashoard/Jobs.vue
+++ household/src/views/User-Dashoard/Jobs.vue
@@ -5,10 +5,10 @@
          <h1 v-else-if="user && user.accountType === 'Homeowner'" class="text-4xl font-bold text-center text-gray-900">Job Listing</h1>
        </div>
 
-         <div v-if="HandelSuccess" class="max-w-7xl mx-auto bg-blue-100 border border-blue-400 text-blue-700 px-4 py-3 rounded relative mb-2" role="alert">
+         <div v-if="HandelSuccess" class="max-w-7xl mx-auto bg-blue-100 border border-blue-400 text-blue-700 px-4 mt-10 mx-5 py-3 rounded relative mb-2" role="alert">
             <span class="block sm:inline text-center">{{ HandelSuccess }}</span>
         </div>
-        <div v-if="HandelError" class="max-w-7xl mx-auto bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-2" role="alert">
+        <div v-if="HandelError" class="max-w-7xl mx-auto bg-red-100 border border-red-400 text-red-700 px-4 mt-10 mx-5 py-3 rounded relative mb-2" role="alert">
             <span class="block sm:inline text-center">{{ HandelError }}</span>
         </div>
 
@@ -24,21 +24,21 @@
   
       <div v-if="user && user.accountType === 'Applicant'" class="w-full max-w-7xl mx-auto">
         <div class="grid gap-5 lg:grid-cols-2 px-5">
-          <div  v-for="jobListing in filteredJobListings" :key="jobListing.job_id" class="bg-white rounded-lg shadow-lg mb-8 p-6 hover:shadow-xl transition-shadow duration-300">
+          <div  v-for="jobListing in filteredJobListings" :key="jobListing.job_id" class="bg-white rounded-lg shadow-md mb-8 p-6 transition-shadow duration-300">
             <h2 class="text-2xl font-bold mb-2 text-purple-800 uppercase"><router-link :to="{ name: 'JobListing', params:{ id:jobListing.job_id} }">{{ jobListing.jobTitle }}</router-link></h2>
             <div class="text-gray-600 text-base mb-4 font-bold capitalize">{{ jobListing.location }}</div>
             <p class="text-gray-800 mb-4">{{ jobListing.description }}</p>
             <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-5">
-              <div class="text-gray-600 text-base font-bold">Applicants: {{jobListing.ApplicantNumber}}</div>
-              <div class="text-gray-600 text-base font-bold">{{ formatDate(jobListing.date) }}</div>
-              <div class="text-gray-600 text-base font-bold">Status: <span class="bg-green-200 rounded-full py-1 px-2">{{ jobListing.status }}</span></div> 
-              <div class="text-2xl font-bold text-purple-800">Ksh {{ formatSalary(jobListing.salary) }}</div>
+              <div><span class="text-gray-600 text-base font-bold">Applicants :</span><span class="text-gray-600 text-base">{{jobListing.ApplicantNumber}}</span></div>
+              <div class="text-gray-600 text-md  font-bold">{{ formatDate(jobListing.date) }}</div>
+              <div><span class="text-gray-600 text-base font-bold">Status:</span> <span class="bg-green-200 text-sm rounded-md py-1 px-2">{{ jobListing.status }}</span></div> 
+              <div class="text-md lg:text-2xl font-bold text-blue-800">Ksh {{ formatSalary(jobListing.salary) }}</div>
             </div>
 
             <div class="flex items-center py-5 justify-between">
               <div class="text-base bg-green-600 text-white px-2 py-1 rounded-md mt-3 capitalize">{{ jobListing.timeSchedule}}</div>
               <p v-if="jobListing.status === 'Closed'" class="mt-4 text-sm text-red-400 px-4 py-2 font-medium">Application Closed</p>
-              <button v-else @click="ApplyForJob(jobListing.job_id)" class="mt-4 bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition-colors duration-300">Apply Now</button>
+              <button v-else @click="ApplyForJob(jobListing.job_id)" class="mt-4 bg-purple-600 text-white px-2 py-1  lg:px-4 lg:py-2 rounded-md hover:bg-purple-700 transition-colors duration-300">Apply Now</button>
               
             </div>
           </div>
@@ -47,20 +47,20 @@
 
       <div v-else-if="user && user.accountType === 'Homeowner'" class="w-full max-w-7xl mx-auto">
         <div class="grid gap-5 lg:grid-cols-2 px-5">
-          <div v-for="jobListing in filteredJobListings" :key="jobListing.job_id" class="bg-white rounded-lg shadow-lg mb-8 p-6 hover:shadow-xl transition-shadow duration-300">
+          <div v-for="jobListing in filteredJobListings" :key="jobListing.job_id" class="bg-white rounded-lg shadow-md mb-8 p-6  transition-shadow duration-300">
             <h2 class="text-2xl font-bold mb-2 text-purple-800 uppercase"><router-link :to="{ name: 'JobListing', params:{ id:jobListing.job_id} }">{{ jobListing.jobTitle }}</router-link></h2>
             <div class="text-gray-600 text-base mb-4 font-bold capitalize">{{ jobListing.location }}</div>
             <p class="text-gray-800 mb-4">{{ jobListing.description }}</p>
             <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-5">
               <div class="text-gray-600 text-base font-bold"><router-link :to="{ name: 'ApplicantJobListing', params:{ id:jobListing.job_id} }">Applicants: {{jobListing.ApplicantNumber}}</router-link></div>
               <div class="text-gray-600 text-base font-bold">{{ formatDate(jobListing.date) }}</div>
-              <div class="text-gray-600 text-base font-bold">Status: <span class="bg-green-200 rounded-full py-1 px-2 ">{{ jobListing.status }}</span></div> 
-              <div class="text-2xl font-bold text-purple-800">Ksh {{ formatSalary(jobListing.salary) }}</div>
+              <div><span class="text-gray-600 text-base font-bold">Status:</span> <span class="bg-green-200 text-sm rounded-md py-1 px-2">{{ jobListing.status }}</span></div> 
+              <div class="text-md lg:text-2xl font-bold text-blue-800">Ksh {{ formatSalary(jobListing.salary) }}</div>
             </div>
 
             <div class="flex items-center py-5 justify-between">
              <div class="text-base bg-green-600 text-white px-2 py-1 rounded-md mt-3 capitalize">{{ jobListing.timeSchedule}}</div>
-             <router-link :to="{ name: 'JobListing', params:{ id:jobListing.job_id} }">  <button class="mt-4 bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition-colors duration-300">More</button></router-link>
+             <router-link :to="{ name: 'JobListing', params:{ id:jobListing.job_id} }">  <button class="mt-4 bg-purple-600 text-white px-2 py-1  lg:px-4 lg:py-2 rounded-md hover:bg-purple-700 transition-colors duration-300">More</button></router-link>
             </div>
           </div>
         </div>
@@ -119,16 +119,23 @@
   },
   methods: {
     ApplyForJob(job_id) {
-    apiCall('/JobsApplication', 'post', {
+      apiCall('/JobsApplication', 'post', {
         ApplicantNumber: this.Applicant,
         job_id: job_id,
       })
       .then((res) => {
-           this.HandelSuccess = "You have Applied For The Job"
-      }) .catch((error) => {
+        this.HandelSuccess = "You have Applied For The Job";
+        this.successTimeout = setTimeout(() => {
+          this.HandelSuccess = '';
+        }, 3000); 
+      })
+      .catch((error) => {
         if (error.response?.status === 400) {
-          this.HandelError = "You have already applied for this job"
-          }
+          this.HandelError = "You have already applied for this job";
+          this.errorTimeout = setTimeout(() => {
+            this.HandelError = '';
+          }, 3000); 
+        }
       });
     },
 
